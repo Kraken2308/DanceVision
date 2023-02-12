22e4e9b2bbe748359c566c71c69f837dda5ffac6
Angan Samadder <rio.angan@gmail.com>
2023-02-12 03:14:29 -0500

    Edit PercentError.py and Compare.py

--- PercentError.py
+++ PercentError.py
@@ -11,7 +11,7 @@ def get_percent_error(inputA, inputB):
 
     for i in range(10):
         percentError = abs(((inputA[i] - inputB[i]) / inputA[i])) * 100
-        temp.append(percentError)
+        temp.append(round(percentError, 3))
 
     return temp
 
@@ -20,19 +20,25 @@ def compare_angle_lists(coach_List, player_List, path):
 
     percent_error_list = []
     flagged_timestamps = []
+    netAccuracy = 0
 
     #algorithm for comparing two node angle lists
 
-    for i in path:
+    for i in range(len(path)):
         percent_error = get_percent_error(coach_List[path[i][0]], player_List[path[i][1]]) 
         percent_error_list.append(percent_error) 
 
-        for j in percent_error:
+        for j in range(len(percent_error)):
+
+            netAccuracy += percent_error[j]
+
             if percent_error[j] >= leeway_Percentage:
                 timestamp = j * 1/30 # check 30 to make sure its fps
-                flagged_timestamps.append(timestamp)
+                flagged_timestamps.append(round(timestamp, 3))
+
+    netAccuracy = netAccuracy / (len(path) * 10)
     
-    return percent_error_list, flagged_timestamps
+    return [percent_error_list, flagged_timestamps, netAccuracy]
 
 
     
